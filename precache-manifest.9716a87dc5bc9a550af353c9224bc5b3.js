02a65073ad1d38eb410012077ee12128d1b43957
Hiteshshingala <51263039+Hiteshshingala@users.noreply.github.com>
2020-06-29 22:19:01 +0530

    first commit

---
+++ precache-manifest.9716a87dc5bc9a550af353c9224bc5b3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c86fa2b323b21e54dc8bbd1eecd5a518",
+    "url": "./index.html"
+  },
+  {
+    "revision": "68fbc979d66c9ed3d847",
+    "url": "./static/css/3.9de105d4.chunk.css"
+  },
+  {
+    "revision": "da66edf2721bb3919be7",
+    "url": "./static/css/main.8372e5fb.chunk.css"
+  },
+  {
+    "revision": "d97f532739ed3d6afa39",
+    "url": "./static/js/2.afdc6a9d.chunk.js"
+  },
+  {
+    "revision": "22521b678ef14f5f5d937053cb39c485",
+    "url": "./static/js/2.afdc6a9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68fbc979d66c9ed3d847",
+    "url": "./static/js/3.dde615c0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/3.dde615c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb07079aa76f79c6d1ec",
+    "url": "./static/js/4.7e45d6d5.chunk.js"
+  },
+  {
+    "revision": "da66edf2721bb3919be7",
+    "url": "./static/js/main.9a3b9a9e.chunk.js"
+  },
+  {
+    "revision": "89f3cc45fe748f7131a7",
+    "url": "./static/js/runtime-main.e1027246.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
